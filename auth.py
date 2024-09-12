b088368a87156e2ec414560b1df40d9132127c64
Ethan Henrickosn <henricksonethan@gmail.com>
2024-09-11 21:22:10 -0500

    Update auth.py
    Firstly, a function should only be responsable for a single task, so I took out the account information and made that into its own function. I then simplified the syntax in this new function so that it is much easier to read.
    
    Also I combinded the Username and Password look up into only one if statement to reduce nesting.
    
    I would call this 2 fixes.

--- auth.py
+++ auth.py
@@ -2,6 +2,7 @@ import json
 from user import User
 from account import Account
 from message import Message
+import time
 
 class Auth:
     def __init__(self):
@@ -12,15 +13,19 @@ class Auth:
             users = json.load(file)['users']
 
         for user in users:
-            if user['name'] == username:
-                if user['password'] == password:
-                    accounts = {}
-                    for account in user['accounts']:
-                        accounts[account['accountNumber']] = Account(account['balance'])
-                    return User(user['name'], accounts)
-                else:
-                    self.message.print("name or password not found")
+            if user['name'] == username and user['password'] == password:
+                accounts = self.get_accounts(user["accounts"])
+                return User(user['name'], accounts)
             else:
                 self.message.print("name or password not found")
             
         return None
+
+    def get_accounts(self, allAccounts):
+        returnAccounts = {}
+        for account in  allAccounts:
+            accountNumber = account['accountNumber']
+            balance = account['balance']
+            
+            returnAccounts[accountNumber] = Account(balance)
+        return returnAccounts
